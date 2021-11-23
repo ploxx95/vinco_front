69cd81577d4ed3f71365c447dd2a218479a78fe3
Alexander-Valladares <ploxx95@gmail.com>
2021-11-23 03:21:12 -0500

    modalwindow created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { CardUser } from "./components/CardUser";
 import { CardWrapper } from "./components/CardWrapper";
+import { ModalWindow } from "./components/ModalWindow";
 import useFetch from "./hooks/useFetch";
 
 function App() {
   const { data, loading, error } = useFetch("/users");
   console.log("DATAA : ", data?.data[0].id);
   return (
-    <div className=" bg-tertiary-100 ">
+    <div className=" bg-tertiary-100 relative ">
       <div className=" container mx-auto grid place-content-center min-h-screen">
         <div className="grid-container">
           <CardWrapper />
@@ -26,6 +27,7 @@ function App() {
           </>
         </div>
       </div>
+      <ModalWindow />
     </div>
   );
 }
--- src/components/modalWindow.js
+++ src/components/modalWindow.js
@@ -1,5 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { CtaButton } from "./CtaButton";
+import { InputLabel } from "./InputLabel";
 
-export const modalWindow = () => {
-  return <div></div>;
+export const ModalWindow = () => {
+  const [value, setValue] = useState({
+    name: "",
+    role: "",
+    score: 0,
+    age: "",
+    course: 0,
+  });
+
+  function handleChange(e) {
+    const input_value = e.target;
+    setValue((prev) => {
+      return {
+        ...prev,
+        [input_value.name]: input_value.value,
+      };
+    });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(value);
+  }
+  return (
+    <div className="absolute top-0 bg-gray-900 h-full w-full bg-opacity-30 grid place-content-center">
+      <div className="px-16 py-12 min-w-min  bg-white rounded-xl border-primary-100 border-2">
+        <form onSubmit={handleSubmit}>
+          <InputLabel
+            value={value.name}
+            handleChange={handleChange}
+            type="text"
+            placeholder="Cual es tu nombre?"
+            name="name"
+            label="Nombre"
+          />
+
+          <InputLabel
+            value={value.role}
+            handleChange={handleChange}
+            type="text"
+            placeholder="Escribe tu Rol"
+            name="role"
+            label="Cargo"
+          />
+
+          <InputLabel
+            value={value.age}
+            handleChange={handleChange}
+            type="date"
+            placeholder="Birthay"
+            name="age"
+            label="Birthay"
+          />
+          <div className="flex gap-4">
+            <InputLabel
+              value={value.score}
+              handleChange={handleChange}
+              type="number"
+              placeholder="Your Score"
+              name="score"
+              label="Puntaje"
+            />
+
+            <InputLabel
+              value={value.course}
+              handleChange={handleChange}
+              type="number"
+              placeholder="Your Score"
+              name="course"
+              label="Cursos"
+            />
+          </div>
+          <CtaButton>Actualizar</CtaButton>
+        </form>
+        ;
+      </div>
+    </div>
+  );
 };
